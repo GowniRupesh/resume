6321b3590ba3fae193b3648880ac34e5210cebbe
sureshnl <nallachi7@gmail.com>
2018-08-01 11:34:55 +0530

    push

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ var fetchedData=loadJSON("data.json");
 fetchedData.then(data=>{
   console.log(data);
   career(data.career);
+  education(data.education);
 })
 
 
@@ -21,8 +22,23 @@ fetchedData.then(data=>{
     var heading=document.createElement("h2");
     heading.textContent="Career Objevtive";
     child2.appendChild(heading);
+    var hLine=document.createElement("hr");
+    child2.appendChild(hLine);
       var p=document.createElement("p");
     p.textContent=car.info;
     child2.appendChild(p);
 
+
+}//Education
+function education(edu){
+var heading=document.createElement("h2");
+heading.textContent="Education Qualification";
+child2.appendChild(heading);
+var hLine=document.createElement("hr");
+child2.appendChild(hLine);
+var table=document.createElement("table");
+child2.appendChild(table);
+var tr="<tr><td>S.NO</td><td>degree</td><td> institute</td><td> Data</td></tr>"
+table.innerHTML=tr;
+table.border="1";
 }
